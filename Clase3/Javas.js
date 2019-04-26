df28f25be534aa152c3107e2fe5483d095f39c62
julietAGuti <juliethaguti@gmail.com>
2019-04-26 15:29:23 -0300

    Add files via upload

--- Clase3/Javas.js
+++ Clase3/Javas.js
@@ -1,53 +1,59 @@
-window.addEventListener("load",function(){
-    var boton = document.getElementById("btn");
-    boton.addEventListener("click",agregar); //Agregar el boton agregar
+window.addEventListener("load", function(){
+    abrir(false);
+    /*var btnAgregar = document.getElementById("btnAgregar");
+    btnAgregar.addEventListener("click",agregar); //Agrega el boton agregar*/
 });
 
-function $(id){
+/*function $(id){
     return document.getElementById(id);
-}
+}*/
 
 function agregar(){
-    var nombre = $("Nombre").value;
-    var apellido = $("Apellido").value;
-    // var apellido = document.getElementById("nombre").value;
-    // var nombre = document.getElementById("apellido").value;
-    if(apellido == "" || nombre == "")
-    {
-        document.getElementById("Nombre").className="error";
-        document.getElementById("Apellido").className="error";
-        alert("Debe ingresar un nombre y un apellido");
-
-        return;
-    }
-    var respuesta = confirm("Esta seguro que desea agregar a un persona?");
-    if(respuesta == true)
-    {
-        document.getElementById("Nombre").className="sinError";
-        document.getElementById("Apellido").className="sinError";
-        var tCuerpo = document.getElementById("tCuerpo");
-
-        tCuerpo.innerHTML = tCuerpo.innerHTML +
-        "<td>"+nombre+"</td>"+
-        "<td>"+apellido+"</td>"+
-        "<td><a href=''borrar</a></td>";
-        cerrar();
-    }
+    abrir(true);
+    limpiar();
 }
 
-function abrir(){
-    var contAgregar = document.getElementById("contAgregar");
-    var btn = document.getElementById("btn");
-    btn.hidden = true;
+function abrir(mostrar){
+    var contAgregar = document.getElementById("contenedor");
+    var btnAgregar = document.getElementById("btnAgregar");
+    btnAgregar.hidden = mostrar;
 
-    contAgregar.hidden = false;
+    contAgregar.hidden = !mostrar;
+}
+
+function guardar(){
+    //var nombre = $("Nombre").value;
+    //var apellido = $("Apellido").value;
+    var nombre = document.getElementById("Nombre").value;
+    var apellido  = document.getElementById("Apellido").value;
+
+    if(apellido == "" || nombre == "")
+    {
+        document.getElementById("Nombre").className="conError";
+        document.getElementById("Apellido").className="conError";
+        alert("Debe ingresar un nombre y un apellido");
+        //return;
+    }
+    else{
+        var respuesta = confirm("Esta seguro que desea agregar a un persona?");
+        if(respuesta == true)
+        {
+            document.getElementById("Nombre").className="sinError";
+            document.getElementById("Apellido").className="sinError";
+            var tCuerpo = document.getElementById("cuerpoTabla");
+
+            tCuerpo.innerHTML = tCuerpo.innerHTML +
+            "<tr><td>"+nombre+"</td><td>"+apellido+"</td><td><a href=''>borrar</a></td>";
+            cerrar();   
+        }
+    }
 }
 
 function cerrar(){
-    var contAgregar = document.getElementById("contAgregar");
-    var btn = document.getElementById("btn");
-    //contAgregar.removeAttribute(LaClaseQueQuiero remover,laotra)
-    document.getElementById("Nombre").value = "";
-    document.getElementById("Apellido").value = "";
+    abrir(false);
 }
 
+function limpiar(){
+    document.getElementById("Nombre").value="";
+    document.getElementById("Apellido").value="";
+}
