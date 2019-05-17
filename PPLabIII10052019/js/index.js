bfe81f9e1659c1162d5f3e4b173cd168191f785d
julietAGuti <juliethaguti@gmail.com>
2019-05-17 17:33:54 -0300

    Add files via upload

---
+++ PPLabIII10052019/js/index.js
@@ -0,0 +1,81 @@
+var xhr = new XMLHttpRequest();
+
+window.addEventListener("load",function(){
+    mostrarFormulario(true);
+    xhr.open("GET","http://localhost:3000/materias",true);
+    xhr.onreadystatechange = callback;
+    xhr.send();
+    var btnCerrar = document.getElementById("btnCerrar");
+    btnCerrar.addEventListener("click",cerrar);
+});
+
+function callback(){
+    if(xhr.readyState === 4 && xhr.status === 200){
+
+        var respuesta = xhr.responseText;
+        var array = JSON.parse(respuesta);
+        var largo = array.lenght;
+        cargar(array);
+    }
+    else{
+        console.log("ERROR"+xhr.status);
+    }
+}
+
+function cargar(respuesta){
+    var tabla = document.createElement("table");
+    document.getElementById("detalleBackground").appendChild(tabla);
+    var cabezaTabla = document.createElement("thead");
+    tabla.appendChild(cabezaTabla);
+    var nombreTitulo = document.createElement("th");
+    cabezaTabla.appendChild(nombreTitulo);
+    nombreTitulo.appendChild(document.createTextNode("Nombre"));
+    var cuatriTitulo = document.createElement("th");
+    cabezaTabla.appendChild(cuatriTitulo);
+    cuatriTitulo.appendChild(document.createTextNode("Cuatrimestre"));
+    var fechaF = document.createElement("th");
+    cabezaTabla.appendChild(fechaF);
+    fechaF.appendChild(document.createTextNode("Fecha Final"));
+    var turnoTitulo = document.createElement("th");
+    cabezaTabla.appendChild(turnoTitulo);
+    turnoTitulo.appendChild(document.createTextNode("Turno"));
+    var cuerpoTabla = document.createElement("tbody");
+    tabla.appendChild(cuerpoTabla);
+    var count = respuesta.lenght;
+
+    for(var i = 0; i<15;i++){
+        var fila = document.createElement("tr");
+        cuerpoTabla.appendChild(fila);
+        fila.addEventListener("dblclick",dobleClick);
+        var columna = document.createElement("td");
+        fila.appendChild(columna);
+        var nombre = document.createTextNode(respuesta[i].nombre);
+        columna.appendChild(nombre);
+        var columnaCuatri = document.createElement("td");
+        fila.appendChild(columnaCuatri);
+        var cuatrimestre = document.createTextNode(respuesta[i].cuatrimestre);
+        columnaCuatri.appendChild(cuatrimestre);
+        var columnaFecha = document.createElement("td");
+        fila.appendChild(columnaFecha);
+        var fechaFinal = document.createTextNode(respuesta[i].fechaFinal);
+        columnaFecha.appendChild(fechaFinal);
+        var columnaTurno = document.createElement("td");
+        fila.appendChild(columnaTurno);
+        var turno = document.createTextNode(respuesta[i].turno);
+        columnaTurno.appendChild(turno);
+    }
+}
+
+function mostrarFormulario(mostrar){
+    var Formulario = document.getElementById("contenedor");
+    Formulario.hidden = mostrar;
+}
+
+function dobleClick(){
+    mostrarFormulario(false);
+    
+}
+
+function cerrar(){
+    mostrarFormulario(true);
+}
